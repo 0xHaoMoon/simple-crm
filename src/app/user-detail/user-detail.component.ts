8986eda2033fcaf4919c3f2e91cba06c22241593
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-09-26 13:21:53 +0200

    get user

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit,inject } from '@angular/core';
-import { Firestore} from '@angular/fire/firestore';
+import { Firestore, doc, docData} from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
 
 
 @Component({
@@ -11,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class UserDetailComponent implements OnInit{
   firestore: Firestore = inject(Firestore);
   userId:any;
+  user:User = new User();
 
   constructor(private route: ActivatedRoute) {}
 
@@ -18,7 +20,17 @@ export class UserDetailComponent implements OnInit{
     this.route.paramMap.subscribe( paramMap => {
       this.userId = paramMap.get('id');
       console.log('got id:', this.userId)
+      this.getUser();
     })
   }
 
+  getUser() {
+    let db = doc(this.firestore, `users/${this.userId}`);
+    
+    docData(db).subscribe((user: any) => {
+      this.user = new User(user);
+      console.log(this.user);
+    })
+  } 
+ 
 }
