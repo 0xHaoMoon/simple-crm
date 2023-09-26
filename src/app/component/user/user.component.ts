38a3841519d602fadc37f36f3a7b6b70125bd837
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-09-26 09:04:47 +0200

    ng for show user

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -2,7 +2,8 @@ import { Component,inject } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from 'src/models/user.class';
-import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -13,10 +14,18 @@ export class UserComponent {
 
   user: User = new User()
   firestore: Firestore = inject(Firestore)
-  db;
+  users$: Observable<any>;
+  allUsers:any;
 
   constructor(public dialog: MatDialog){
-    this.db = collection(this.firestore, 'users');
+    const db = collection(this.firestore, 'users');
+    this.users$ = collectionData(db, { idField: 'id' });
+    console.log(this.users$);
+    
+
+    this.users$.subscribe((changes) => {
+      this.allUsers = changes;
+    });
     
   }
   
