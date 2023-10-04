8cb88d7b88c3a1981a2fca71a9cf42ba01fed447
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-10-04 21:31:44 +0200

    fix layout login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +11,18 @@ export class LoginComponent {
   hide = true;
   email = new FormControl('', [Validators.required, Validators.email]);
 
+
+  constructor(private router: Router) { }
+
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  guestLog(){
+      this.router.navigate(['dashboard']);
   }
 }
