c9d1ae083463c40e1416537c9da04943f3d949e2
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-10-05 09:54:23 +0200

    login with firebase authen

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
-import { getAuth } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-login',
@@ -9,22 +10,66 @@ import { getAuth } from "firebase/auth";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  firestore: Firestore = inject(Firestore)
   hide = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email!: string;
+  password!:string;
  
 
 
   constructor(private router: Router) { }
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
 
   guestLog(){
       this.router.navigate(['dashboard']);
   }
+
+  logIn(){
+    const auth = getAuth();
+signInWithEmailAndPassword(auth, this.email, this.password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log('Log In successfully' + user)
+    this.router.navigate(['/dashboard']);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode)
+    console.log(errorMessage);
+    alert("wrong email or passwort")
+    this.email = ''
+    this.password = ''
+  });
+  }
+
+  
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  getEmailErrorMessage() {
+    if (this.emailFormControl.hasError('required')) {
+      return 'You must enter a e-mail';
+    }
+    return this.emailFormControl.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6), 
+  ]);
+  getPasswordErrorMessage() {
+    if (this.passwordFormControl.hasError('required')) {
+      return 'You must enter a password';
+    }
+    return this.passwordFormControl.hasError('minlength')
+      ? 'Must be at least 6 characters'
+      : '';
+  }
+
 }
+
+
+
